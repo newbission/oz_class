1ff876a53258712651123c080363f43d953d5b29
newbission <newbission@gmail.com>
2024-02-02 21:53:18 +0900

    Study: 데이터베이스 2일차 실습 결과물

---
+++ basic/class/database/mysql/RDBMS실습1-간단한_스키마_설계하기.sql
@@ -0,0 +1,22 @@
+USE testdatabase;
+CREATE TABLE users(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    password VARCHAR(4),
+    name VARCHAR(3),
+    gender ENUM('male', 'female'),
+    email VARCHAR(15),
+    birthday CHAR(6),
+    age TINYINT,
+    company ENUM('samsung', 'lg', 'hyundai')
+);
+
+CREATE TABLE boards(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(5),
+    content VARCHAR(10),
+    likes INT CHECK(likes BETWEEN 1 AND 100),
+    img CHAR(1) DEFAULT 'c',
+    created DATE,
+    user_id int,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
